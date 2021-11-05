8c6b462ecbca37bdf21c28e0e45c27981d42aeed
EDWIN CIPRIAN <93774187+edwinmatosp@users.noreply.github.com>
2021-11-05 08:45:44 -0400

    Add files via upload

---
+++ LotteryPin.py
@@ -0,0 +1,101 @@
+print("Bienvenidos al sistema de predicción de las loterias: REAL, NYD, GM, LOTEKA, NYN, LEISDA Y LNN")
+
+
+a1=int(input("Indique el valor de a1:"))
+a2=int(input("Indique el valor de a2:"))
+a3=int(input("Indique el valor de a3:"))
+a4=int(input("Indique el valor de a4:"))
+
+b1=int(input("Indique el valor de b1:"))
+b2=int(input("Indique el valor de b2:"))
+b3=int(input("Indique el valor de b3:"))
+b4=int(input("Indique el valor de b4:"))
+
+c1=int(input("Indique el valor de c1:"))
+c2=int(input("Indique el valor de c2:"))
+c3=int(input("Indique el valor de c3:"))
+c4=int(input("Indique el valor de c4:"))
+
+
+if a1==b2:
+    print(b1,b2)
+
+if b2-a2==1:
+    print(b2,b1)
+
+
+if a2==b2: ("día de mañana. Validado para NYN 6 ENERO 2020")
+if a1==b1:
+    print(b1,b2)
+    print("Para el día de hoy. Validado para NYD 18 sep 2019")
+
+    print(b1,b2,b2,b3)
+    print("Para el día de hoy. Validado para 20 sep 2019 GM")
+
+if a2==b2:
+    print(a1,a2)
+    print("Para el día de hoy. Validado para 20 sep 2019 GM")
+    
+if a1==b2:
+    print(a1,a2)
+    print("Para el día de hoy. Validado para 10 JUNIO 2020 LR")
+
+if a3==c3:
+    print(b3,c3)
+    print("Para el día de hoy. Validado para 29 ENERO 2020 GM, OPCION 1")
+
+
+if b3==c2:
+    print(b3,b2)
+    print("Para el día de hoy. Validado para 27 ENERO 2020 GM, OPCION 1")
+
+if a2==b3:
+    print(a2,a3)
+    print("Para el día de hoy. Validado para 06 ENERO 2020 NYD, OPCION 1")
+
+if a2==b4:
+    print(a2,b3)
+    print("Para el día de hoy. Validado para 30 NOV 2019 NYD, OPCION 1")
+
+
+if a2==b3:
+    print(b2,b3)
+    print("Para el día de mañana. Validado para 18 DIC 2019 NYD, OPCION 1")
+
+if b3-b4==-1:
+    print(a4,b4)
+    print("SUMARLE 1 A ESTE RESULTADO Para el día de hoy. Validado para 25 ENE 2020 LNN, OPCION 1")
+
+if b4==c3:
+    print(c4,c3)
+    print(" Para el día de hoy. Validado para 11 OCT 2019 NYN, OPCION 1")
+
+
+if a2==b2:
+    print(b2,b3)
+    print("Para el día de hoy. Validado para 14 OCT 2019 GM, OPCION 1")
+
+
+if a1==b1:
+    print(a2,b1)
+    print("Para el día de hoy. Validado para 15 OCT 2019 NYN, OPCION 1")
+
+
+if b2==c2:
+    print(b2,b1)
+    print("Para el día de hoy. Validado para 15 OCT 2019 NYN, DATERO POPULAR")
+
+if b4==c3:
+    print(b3,b4)
+    print("Para el día de hoy. Validado para 15 OCT 2019 NYN, DATERO POPULAR")
+
+
+if a1==b1:
+    print(a2,a1)
+    print("Para el día de hoy. Validado para 24 DIC 2019 NYT, DATERO POPULAR")
+
+if b3-b4==1:
+    print(a3,b4)
+    print("Es buena para LTK y QR, si no sale en la fecha al otro dia puede ser buena opcion")
+
+    
